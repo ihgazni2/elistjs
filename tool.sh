400adad5db508d342a0501c6f293497f545a0050
dli <ihgazni2010@hotmail.com>
2019-08-25 15:44:17 +0000

    docs

--- tool.sh
+++ tool.sh
@@ -1,3 +1,9 @@
 cat elist.js | egrep function | sed -r "s/function (.*)\((.*)\) \{/\1/"
 
 npm install jsdoc -g
+jsdoc elist.js -d docs
+
+webpack
+
+#how to bind a domain name 
+# https://www.jianshu.com/p/a2b647f4c999
